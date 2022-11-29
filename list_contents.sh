15886c6c75fce87e1e6d213cdefafd26c99eaf08
bhargavmyadam <74249805+bhargavmyadam@users.noreply.github.com>
2022-11-29 10:06:16 +0530

    Update list_contents.sh

--- list_contents.sh
+++ list_contents.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/system/bin/sh
 #timestamp stores the current time and date
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 echo "timestamp = $timestamp"
 #deviceId stores the current tabletId
-deviceId=$(getprop ro.serialno)
+deviceId=$(id -u)
 echo "deviceId = $deviceId"
 #filename is used for identifying the file and is of the format "timestamp_deviceId"
 FILES_fileName="FILES_${deviceId}_${timestamp}.txt"
 echo "FILES_fileName = $FILES_fileName"
 #command to recursively list the subfolders inside the given folder
-out=$(ls -R /mnt/sdcard/RoboTutor)
+out=$(ls -R /sdcard/Download)
 #command to create a new text file with given fileName as the name and redirecting the contents of out into the file
-echo "$out" > /mnt/sdcard/RoboTutor/"$FILES_fileName"
+echo "$out" > /sdcard/RoboTutor/"$FILES_fileName"
